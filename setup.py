9be62331e4c857505e824ce416f97976989a4331
CRYO x ZeN_oH <barmansubha02@gmail.com>
2025-07-09 10:45:22 +0530

    Added all SwiggySense Folders to Git

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Build Ml project that predict food delivery time in minutes',
+    author='Aniket',
+    license='',
+)
